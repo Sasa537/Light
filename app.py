e27bd8f5277edcce309dec6ba5c33a709257fdb3
Sasa537 <sasa5375632@gmail.com>
2022-07-16 19:57:31 +0300

    Полность готовый WEB проект

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template,redirect
-from rooms import db
+from flask import Flask, render_template, redirect, request
+from rooms import db, Room
 
 app = Flask(__name__)
 app.secret_key = 'xyz'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///light.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -18,14 +18,46 @@ db.init_app(app)
 
 @app.route("/")
 def index():
+    room = Room.query.order_by(Room.id.desc()).all()
+    return render_template("index.html", room=room)
 
-    return render_template("index.html")
 
-
-@app.route("/add")
+@app.route("/add",  methods=['POST', 'GET'])
 def add():
+    if request.method == "POST":
+        room_n = request.form['name_room']
+        room = Room(room_name=room_n, lights=1)
+        try:
+            if room:
+                db.session.add(room)
+                db.session.commit()
 
-    return render_template("add.html")
+                return redirect('/')
+
+            else:
+                return redirect('/add')
+
+        except:
+            return "Error adding"
+
+    else:
+        return render_template("add.html")
+
+
+@app.route("/<int:id>", methods=['POST', 'GET'])
+def on_off(id):
+    room = Room.query.get(id)
+    if room.lights == 1:
+        room.lights = 0
+    elif room.lights == 0:
+        room.lights = 1
+    try:
+        db.session.commit()
+        return redirect('/')
+
+    except:
+        return "При редактировании статьи произошла ошибка"
+    return redirect("/")
 
 
 app.run(host='localhost', port=5000)
--- rooms.py
+++ rooms.py
@@ -6,5 +6,9 @@ db = SQLAlchemy()
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room_name = db.Column(db.String(100))
+    lights = db.Column(db.Integer)
 
 
+def __init__(self, room_name, lights):
+    self.room_name = room_name
+    self.lights = lights
